936589e93970d6260fc11c7df4d3c1a8dddeaefd
Tharnid <jeffmsmithdev@gmail.com>
2016-09-18 21:30:36 -0400

    server response and dice roll

--- LGQL/schema/main.js
+++ LGQL/schema/main.js
@@ -1,8 +1,12 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
 } = require ('graphql');
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
 
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
@@ -10,13 +14,25 @@ const queryType = new GraphQLObjectType({
     hello: {
       type: GraphQLString,
       resolve: () => 'world'
+    },
+    // diceRoll: {
+    //   args: {
+    //     count: {
+    //       type: GraphQLInt,
+    //       defaultValue: 2
+    //     }
+    //   }
+    // }
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => [roll(), roll()]
     }
   }
 });
 
 const mySchema = new GraphQLSchema({
   // root query & root mutation definitions
-  query = queryType
+  query: queryType
 });
 
 module.exports = mySchema;
