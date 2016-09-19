3cd171ef3d3a168ee68dd20de0a6dee056dde6fc
Tharnid <jeffmsmithdev@gmail.com>
2016-09-18 21:15:52 -0400

    basic client call and server response

--- LGQL/schema/main.js
+++ LGQL/schema/main.js
@@ -3,3 +3,20 @@ const {
   GraphQLObjectType,
   GraphQLString
 } = require ('graphql');
+
+const queryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
+    }
+  }
+});
+
+const mySchema = new GraphQLSchema({
+  // root query & root mutation definitions
+  query = queryType
+});
+
+module.exports = mySchema;
